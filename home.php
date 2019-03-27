6605f27ca7b76f13ee4551cf2950db8b7d3b20d6
pritam775558 <pritam1994@me.com>
2019-03-27 13:37:04 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Flight Booking Website </title>
+</head>
+<body>
+
+<div class= "header">
+
+	<h1> Book a Travel</h1>
+	
+</div>
+
+<h1>Welcome to Homepage</h1>
+
+
+<div>
+	
+</div>
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Flight Booking Website </title>
+</head>
+<body>
+
+<div class= "header">
+
+	<h1> Book a Travel</h1>
+	
+</div>
+
+<form> method= "post" action= "login.php">
+	<table>
+		<tr>
+			<td>Username:</td>
+			<td>input type="text" name="username" class="textInput"></td>
+
+		</tr>
+
+		<tr>
+			<td>Password:</td>
+			<td>input type="password" name="password" class="textInput"></td>
+
+		</tr>
+
+		<tr>
+			<td></td>
+			<td>input type="submit" name="login_btn" value="Login"></td>
+
+		</tr>
+	</table>
+</form>
+
+
+
+
+
+</body>
+</html>
