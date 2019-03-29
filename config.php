1ca15907623f4ad9af33d279c33b30a3d153f6df
Necro991 <paladin@Pmeb.org>
2019-03-29 11:27:54 -0400

    Added config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>PHP Config</title>
+	</head>
+	<body>
+		<?php
+			$servername="localhost";
+			$username="agould5";
+			$password="agould5";
+			$dbname="agould5";
+			
+			//Create Connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			
+			//Check Conncetion
+			if ($conn->connect_error){
+				die("Connection failed:".$conn->connect_error);
+			}
+			echo "Connected Successfully!";
+			?>
+	</body>
+</html>
